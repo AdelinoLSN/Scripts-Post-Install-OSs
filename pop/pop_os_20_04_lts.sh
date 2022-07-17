a2dc555ae2e8f93477492e90539b2937ee4d27da
Adelino <adelinolsn@gmail.com>
2022-07-17 07:50:13 -0300

    wip

---
+++ pop/pop_os_20_04_lts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir Workspace
+
+sudo apt update
+
+sudo apt purge -y gnome-contacts geary popsicle* gnome-weather gnome-calendar gnome-calculator
+sudo apt purge -y gedit libreoffice-* gnome-user-docs*
+
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update && sudo apt install sublime-text -y
+
+sudo apt install -y zsh
+chsh -s $(which zsh)
+
+sudo apt install -y git
+
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+sudo apt update
+sudo apt install grub2 grub-customizer os-prober -y
